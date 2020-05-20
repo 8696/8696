2472a2334bb995c8fc554578769949c3b0ae8eff
longjinwen <204084802@qq.com>
2020-05-20 22:20:05 +0800

    Wed May 20 2020 22:20:05 GMT+0800 (China Standard Time)

--- test/001.js
+++ test/001.js
@@ -1,13 +1,3 @@
-/**
- * Created by WebStorm
- * Description: next..
- * User: JinwenLong
- * Author: longjinwen
- * Email: 204084802@qq.com
- * Date: 2020/3/24
- * Time: 9:25 下午
- */
-
 
 
 function Test() {
